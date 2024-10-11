350647022e0f691f0814cfe4b76d9bf613c02f26
Joshdpaul <jdpaul3@alaska.edu>
2024-10-11 08:53:42 -0800

    add note about ingest template

--- build_ingest_json.py
+++ build_ingest_json.py
@@ -1,2 +1,4 @@
 # script to build JSON files for Rasdaman ingest
 # this script should read the attributes of the netCDF files, which will contain a dictionary of the encodings
+
+# this ingest was used a template (minus the spatial components): https://github.com/ua-snap/rasdaman-ingest/blob/7536c17a0df2ecec18c965f030d7e943bccd2d01/iem/gipl2_4km/ingest.json
