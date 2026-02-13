cddd712300696eb5ec2a7c35d7c5c726ae383af3
Craig Stephenson <crstephenson@alaska.edu>
2026-02-13 13:23:00 -0900

    Add error handling for API error codes, very slow responses, or unreachable.

--- webapp/components/Loading.vue
+++ webapp/components/Loading.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import { useStreamSegmentStore } from '~/stores/streamSegment'
 const streamSegmentStore = useStreamSegmentStore()
-let { isLoading } = storeToRefs(streamSegmentStore)
+let { isLoading, apiSlow, apiFailed } = storeToRefs(streamSegmentStore)
 </script>
 <template>
-	<div v-if="isLoading" class="content is-size-4">
-		<p>Loading data&hellip; this can take a minute or two.</p>
-		<progress class="progress" />
-	</div>
+  <div v-if="isLoading" class="content is-size-4">
+    <p v-if="apiSlow">This is taking longer than usual. Hold tight&hellip;</p>
+    <p v-else>Loading data&hellip; this can take a minute or two.</p>
+    <progress class="progress" />
+  </div>
+  <div v-else-if="apiFailed" class="content is-size-4">
+    <p>
+      ⚠️ Unable to access data for this stream. We are working to resolve this
+      issue and apologize for the inconvenience.
+    </p>
+  </div>
 </template>
