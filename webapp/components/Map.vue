f12c3e01c8cf2973d907f68566ecb9f012b67372
Craig Stephenson <crstephenson@alaska.edu>
2026-02-20 16:04:11 -0900

    Add interactive choropleth.

--- webapp/components/Map.vue
+++ webapp/components/Map.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-const { $L } = useNuxtApp()
-import { useStreamSegmentStore } from '~/stores/streamSegment'
+const { $L, $config } = useNuxtApp()
+import simplifiedHucs from '~/assets/hucs_simplified.json'
 
-const streamSegmentStore = useStreamSegmentStore()
+let segBaseUrl = `${$config.public.geoserverUrl}/hydrology/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=hydrology%3Aseg_h8_outlet_stats_simplified&outputFormat=application%2Fjson&srsName=EPSG:4236&cql_filter=huc8=`
+let hucBaseUrl = `${$config.public.geoserverUrl}/hydrology/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=hydrology%3Ahuc8&outputFormat=application%2Fjson&srsName=EPSG:4236&cql_filter=huc8=`
 
-let zoomAddGeoJson = false
-let moveAddGeoJson = false
-let selectedSeg = null // will have a reference to a stream segment Leaflet object
-onMounted(() => {
-  var map = $L
+const defaultMapZoom = 4
+const defaultMapCenter = [37.8, -96]
+
+let map: any
+let wmsLayer: any
+let simplifiedHucsLayer: any
+let simplifiedHucLayer: any
+let detailedHucLayer: any
+let hucSegmentLayers: any[] = []
+let resetButton: any
+
+const initializeMap = () => {
+  map = $L
     .map('map', {
       scrollWheelZoom: false,
+      zoomSnap: 0.1,
     })
-    .setView([37.8, -96], 8)
-  var geoJsonlayer: any = null
+    .setView(defaultMapCenter, defaultMapZoom)
 
   $L.tileLayer(
     'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
@@ -23,108 +32,148 @@ onMounted(() => {
     }
   ).addTo(map)
 
-  var wmsLayer = $L.tileLayer
-    .wms('https://gs.earthmaps.io/geoserver/wms', {
+  wmsLayer = $L.tileLayer
+    .wms(`${$config.public.geoserverUrl}/wms`, {
       transparent: true,
       format: 'image/png',
-      layers: 'hydrology:seg',
+      layers: 'hydrology:huc8_conus_stats_simplified',
+      styles: 'hydrology:hydroviz-choropleth',
       zIndex: 10,
     })
     .addTo(map)
 
-  map.on('zoomend', function () {
-    if (moveAddGeoJson) {
-      return
-    } else {
-      zoomAddGeoJson = true
-    }
-    if (map.getZoom() > 8) {
-      if (!geoJsonlayer) {
-        var bounds = map.getBounds()
-        var minLon = bounds.getWest()
-        var maxLon = bounds.getEast()
-        var minLat = bounds.getSouth()
-        var maxLat = bounds.getNorth()
-        addGeoJson(minLon, maxLon, maxLat, minLat)
-      }
-    } else {
-      if (!map.hasLayer(wmsLayer)) {
-        map.addLayer(wmsLayer)
-      }
-      if (geoJsonlayer && map.hasLayer(geoJsonlayer)) {
-        map.removeLayer(geoJsonlayer)
-        geoJsonlayer = null
-      }
-    }
-  })
+  simplifiedHucsLayer = $L
+    .geoJSON(simplifiedHucs, {
+      style: {
+        opacity: 0,
+        fillOpacity: 0,
+        color: 'transparent',
+        fillColor: 'transparent',
+        weight: 0,
+      },
+      onEachFeature: hucClickHandler,
+    })
+    .addTo(map)
 
-  map.on('moveend', function () {
-    if (zoomAddGeoJson) {
-      return
-    } else {
-      moveAddGeoJson = true
-    }
-    if (geoJsonlayer) {
-      map.removeLayer(geoJsonlayer)
-      geoJsonlayer = null
-    }
-    if (map.getZoom() > 8) {
-      var bounds = map.getBounds()
-      var minLon = bounds.getWest()
-      var maxLon = bounds.getEast()
-      var minLat = bounds.getSouth()
-      var maxLat = bounds.getNorth()
-      addGeoJson(minLon, maxLon, maxLat, minLat)
-    } else {
-      if (!map.hasLayer(wmsLayer)) {
+  resetButton = $L.Control.extend({
+    options: {
+      position: 'topright',
+    },
+    onAdd: function () {
+      const btn = $L.DomUtil.create('resetButton', 'button')
+      btn.innerHTML = 'Reset'
+      $L.DomEvent.disableClickPropagation(btn)
+      btn.addEventListener('click', () => {
+        map.removeControl(this)
+        map.removeLayer(detailedHucLayer)
         map.addLayer(wmsLayer)
-      }
-      if (geoJsonlayer && map.hasLayer(geoJsonlayer)) {
-        map.removeLayer(geoJsonlayer)
-        geoJsonlayer = null
-      }
-    }
-  })
-
-  const addGeoJson = async (
-    minLon: number,
-    maxLon: number,
-    maxLat: number,
-    minLat: number
-  ) => {
-    if (geoJsonlayer) return
-    // "get geojson of matching stream segments"
-    // TODO: switch between a simplified geometry at farther-out zoom levels
-    // to improve performance
-    fetch(
-      'https://gs.earthmaps.io/geoserver/hydrology/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=hydrology%3Aseg&outputFormat=application%2Fjson&srsName=EPSG:4326&cql_filter=INTERSECTS(the_geom,ENVELOPE(' +
-        minLon +
-        ',' +
-        maxLon +
-        ',' +
-        maxLat +
-        ',' +
-        minLat +
-        '))'
-    )
-      .then(response => response.json())
-      .then(data => {
-        geoJsonlayer = $L
-          .geoJSON(data, {
-            style: {
-              weight: 5,
-            },
-          })
-          .addTo(map)
-          .on('click', function (e) {
-            // Navigate to report page
-            navigateTo('/conus/' + e.sourceTarget.feature.properties.seg_id_nat)
-          })
-        zoomAddGeoJson = false
-        moveAddGeoJson = false
-        map.removeLayer(wmsLayer)
+        map.addLayer(simplifiedHucsLayer)
+        hucSegmentLayers.forEach(layer => {
+          map.removeLayer(layer)
+        })
+        hucSegmentLayers = []
+        map.flyTo(defaultMapCenter, defaultMapZoom, {
+          duration: 0.25,
+        })
       })
-  }
+      return btn
+    },
+  })
+}
+
+const adddetailedHucLayer = (data: any) => {
+  let huc = data
+  detailedHucLayer = $L
+    .geoJSON(huc, {
+      style: {
+        weight: 3,
+        color: '#444444',
+        fillOpacity: 0.15,
+      },
+    })
+    .addTo(map)
+  let bounds = $L.geoJSON(huc).getBounds()
+  map.flyToBounds(bounds, {
+    padding: [50, 50],
+    duration: 0.25,
+  })
+}
+
+const addHucSegments = (data: any) => {
+  map.removeLayer(simplifiedHucLayer)
+  // Add each segment individually so we can add hover effects
+  // (color change and tooltip) to each segment individually.
+  data.features.forEach(feature => {
+    const layer = $L
+      .geoJSON(feature, {
+        style: {
+          weight: 4,
+          color: '#0000ff',
+        },
+      })
+      .addTo(map)
+      .on('mouseover', function (e) {
+        e.target.setStyle({
+          color: '#ffff00',
+        })
+        let segmentName = feature.properties.GNIS_NAME
+        if (segmentName.length !== '') {
+          layer.bindTooltip(segmentName).openTooltip()
+        }
+      })
+      .on('mouseout', function (e) {
+        e.target.setStyle({
+          color: '#0000ff',
+        })
+      })
+      .on('click', function (e) {
+        navigateTo('/conus/' + e.sourceTarget.feature.properties.seg_id_nat)
+      })
+    hucSegmentLayers.push(layer)
+  })
+  map.removeLayer(wmsLayer)
+}
+
+const hucClickHandler = (feature: any, layer: any) => {
+  layer.on('click', function () {
+    map.removeLayer(simplifiedHucsLayer)
+    map.addControl(new resetButton())
+    if (feature.properties && feature.properties.huc8) {
+      // Make the simplified HUC-8 visible when it is clicked.
+      // It will be swapped with a high-vertex HUC-8 before zooming in.
+      simplifiedHucLayer = $L
+        .geoJSON(feature, {
+          style: {
+            weight: 3,
+            color: '#444444',
+            fillOpacity: 0.1,
+          },
+        })
+        .addTo(map)
+
+      let segUrl = segBaseUrl + feature.properties.huc8
+      let hucUrl = hucBaseUrl + feature.properties.huc8
+      let segFetch = fetch(segUrl)
+      let hucFetch = fetch(hucUrl)
+
+      Promise.all([segFetch, hucFetch]).then(() => {
+        hucFetch
+          .then(response => response.json())
+          .then(data => {
+            adddetailedHucLayer(data)
+          })
+        segFetch
+          .then(response => response.json())
+          .then(data => {
+            addHucSegments(data)
+          })
+      })
+    }
+  })
+}
+
+onMounted(() => {
+  initializeMap()
 })
 </script>
 
