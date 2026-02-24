74bff07d645fb299b51eeeb0fa55c72b67c56a2f
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-02-23 17:34:27 -0800

    Add data sentences and routing (#91)

--- webapp/README.md
+++ webapp/README.md
@@ -24,3 +24,55 @@ Running the webapp:
 cd webapp
 npm run dev
 ```
+
+## Deploying to production
+
+(work in progress)
+
+### Enable website hosting on the AWS S3 bucket:
+
+**_The following command should be run only during the initial setup of the S3
+bucket. Do not run this command again or it will wipe out the S3 website
+redirection rules._**
+
+```bash
+aws s3 website s3://{{ S3 bucket name }}/ --index-document index.html --error-document index.html
+```
+
+### Add website redirection rules to S3 bucket
+
+Website redirection rules need to be added to the S3 bucket for data to hydrate
+properly when a report is referenced directly by its URL (permalink).
+
+From the AWS web interface for the S3 bucket, go to:
+
+Properties → Static website hosting → Edit
+
+In the "Redirection rules" text area, add the following:
+
+```
+[
+    {
+        "Condition": {
+            "HttpErrorCodeReturnedEquals": "404"
+        },
+        "Redirect": {
+            "HostName": "{{ finalized URL for tool }}",
+            "Protocol": "http",
+            "ReplaceKeyPrefixWith": "#!/"
+        }
+    },
+    {
+        "Condition": {
+            "HttpErrorCodeReturnedEquals": "403"
+        },
+        "Redirect": {
+            "HostName": "{{ finalized URL for tool }}",
+            "Protocol": "http",
+            "ReplaceKeyPrefixWith": "#!/"
+        }
+    }
+]
+```
+
+Code in `layouts/default.vue` catches and routes these redirects into fully hydrated report pages.
---
+++ webapp/components/DataSentences.vue
@@ -0,0 +1,183 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { fnc } from '~/utils/general'
+import { useStreamSegmentStore } from '~/stores/streamSegment'
+const streamSegmentStore = useStreamSegmentStore()
+let { streamStats } = storeToRefs(streamSegmentStore)
+
+function adjectiveForPercent(value) {
+  if (value < -50) {
+    return 'dramatically decrease'
+  } else if (value >= -50 && value < -30) {
+    return 'substantially decrease'
+  } else if (value >= -30 && value < -10) {
+    return 'moderately decrease'
+  } else if (value >= -10 && value < 0) {
+    return 'slightly decrease or stay the same'
+  } else if (value >= 0 && value < 20) {
+    return 'slightly increase or stay the same'
+  } else if (value >= 20 && value < 60) {
+    return 'moderately increase'
+  } else if (value >= 60 && value < 100) {
+    return 'substantially increase'
+  } else if (value >= 100) {
+    return 'dramatically increase'
+  }
+}
+
+// Returns 's' or '' depending on value
+function ifPlural(value) {
+  value = Math.abs(value)
+  if (value == 1) {
+    return ''
+  }
+  return 's'
+}
+
+// Type = 'event' or 'day'
+function blurbFromEventCountOrDuration(historical: number, delta: number, type: string): string {
+  let blurb = ''
+  if (delta < 0) {
+    blurb +=
+      'decrease to ' +
+      (historical - Math.abs(delta)) +
+      ' (' +
+      delta +
+      ' ' +
+      type +
+      ifPlural(delta) +
+      ' per year)'
+  } else if (delta == 0) {
+    blurb += 'stay about the same (' + historical + ' ' + type + 's per year)'
+  } else if (delta > 0) {
+    blurb +=
+      'increase to ' +
+      (historical + Math.abs(delta)) +
+      ' (+' +
+      delta +
+      ' ' +
+      type +
+      ifPlural(delta) +
+      ' per year)'
+  }
+  return blurb
+}
+
+// Whether we think there is strong model disagreement for a min/max range
+const isMeanAnnualFlowHighlyVariable = computed(() => {
+  return (
+    Math.abs(
+      streamStats.value.summary.ma99_delta.range_high -
+        streamStats.value.summary.ma99_delta.range_low
+    ) > 50
+  )
+})
+
+const isMax1DayFlowHighlyVariable = computed(() => {
+  return (
+    Math.abs(
+      streamStats.value.summary.dh1_delta.range_high -
+        streamStats.value.summary.dh1_delta.range_low
+    ) > 50 ||
+    Math.abs(
+      streamStats.value.summary.dl1_delta.range_high -
+        streamStats.value.summary.dl1_delta.range_low
+    ) > 50
+  )
+})
+
+const lowFlow = computed(() => {
+  return streamStats.value.summary.ma99_hist.value <= 5
+})
+
+const sculptedMeanAnnualFlow = computed(() => {
+  if (streamStats.value.summary.ma99_hist.value == 0) {
+    return 'less than 1'
+  }
+  return 'of about ' + fnc(streamStats.value.summary.ma99_hist.value)
+})
+
+function orMoreIf100Percent(amount: number): string {
+  if (amount >= 100) {
+    return ' or more'
+  }
+  return ''
+}
+</script>
+
+<template>
+  <div class="content is-size-5">
+    <p v-if="lowFlow">
+      <strong>Note: this stream segment has a low mean annual flow.</strong>
+      Headwaters and other small or intermittent streams have high statistical
+      variability.
+    </p>
+    <ul>
+      <li>
+        Historically, this stream has a mean annual flow
+        {{ sculptedMeanAnnualFlow }} cf/s. The mean annual flow is projected to
+        {{ adjectiveForPercent(streamStats.summary.ma99_delta.value) }} ({{
+          streamStats.summary.ma99_delta.value
+        }}%{{ orMoreIf100Percent(streamStats.summary.ma99_delta.value) }}) by
+        mid-century.
+        <span v-if="isMeanAnnualFlowHighlyVariable">&#x26A0;&#xFE0F;</span>
+      </li>
+      <li>
+        The maximum 1-day flow is projected to
+        {{ adjectiveForPercent(streamStats.summary.dh1_delta.value) }} ({{
+          streamStats.summary.dh1_delta.value
+        }}%{{ orMoreIf100Percent(streamStats.summary.dh1_delta.value) }}) and
+        the minimum 1-day flow is projected to
+        {{ adjectiveForPercent(streamStats.summary.dl1_delta.value) }} ({{
+          streamStats.summary.dl1_delta.value
+        }}%{{ orMoreIf100Percent(streamStats.summary.dl1_delta.value) }}).
+        <span v-if="isMax1DayFlowHighlyVariable">&#x26A0;&#xFE0F;</span>
+      </li>
+      <li>
+        The mean number of high flood pulse events is projected to
+        {{
+          blurbFromEventCountOrDuration(
+            streamStats.summary.fh1_hist.value,
+            streamStats.summary.fh1_delta.value,
+            'event'
+          )
+        }}, and the high flow pulse duration is projected to
+        {{
+          blurbFromEventCountOrDuration(
+            streamStats.summary.dh15_hist.value,
+            streamStats.summary.dh15_delta.value,
+            'day'
+          )
+        }}.
+      </li>
+      <li>
+        The mean number of low flood pulse events is projected to
+        {{
+          blurbFromEventCountOrDuration(
+            streamStats.summary.fl1_hist.value,
+            streamStats.summary.fl1_delta.value,
+            'event'
+          )
+        }}, and the low flow pulse duration is projected to
+        {{
+          blurbFromEventCountOrDuration(
+            streamStats.summary.dl16_hist.value,
+            streamStats.summary.dl16_delta.value,
+            'day'
+          )
+        }}.
+      </li>
+    </ul>
+    <p v-if="isMeanAnnualFlowHighlyVariable || isMax1DayFlowHighlyVariable">
+      <span class="warn">&#x26A0;&#xFE0F;</span> Model outputs vary by 50% or
+      more for these variables.
+    </p>
+    <p>
+      Historical data uses the Maurer calibration, 1976&ndash;2005. Future
+      projections for mid-century (2046&ndash;2075) use the mean of 13 global
+      circulation models and a middle-of-the-road emissions scenario (RCP 6.0).
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
