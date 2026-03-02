59bdc022ec052eba71377867215802cfb574b045
Craig Stephenson <crstephenson@alaska.edu>
2026-03-02 11:15:19 -0900

    Implement stream segment / HUC search interface.

---
+++ webapp/components/Search.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+const { $autoComplete, $config, $_ } = useNuxtApp()
+const inputValue = ref('')
+const searchInput = ref()
+
+onMounted(() => {
+  let config = {
+    selector: '#search',
+    placeHolder: 'Search for a stream segment or HUC',
+    data: {
+      src: async (query: string) => {
+        let items = []
+
+        const segUrl = `${$config.public.geoserverUrl}/hydrology/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=hydrology%3Aseg_h8_outlet_stats_simplified&outputFormat=application%2Fjson&srsName=EPSG:4326&propertyName=seg_id_nat,GNIS_NAME,GAUGE_ID&cql_filter=GNIS_NAME%20ILIKE%20%27%25${query}%25%27`
+        const hucUrl = `${$config.public.geoserverUrl}/hydrology/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=hydrology%3Ahuc8_conus_stats_simplified&outputFormat=application%2Fjson&srsName=EPSG:4326&propertyName=%20huc8,name&cql_filter=name%20ILIKE%20%27%25${query}%25%27`
+
+        const [segRes, hucRes] = await Promise.all([
+          fetch(segUrl).then(res => res.json()),
+          fetch(hucUrl).then(res => res.json()),
+        ])
+
+        segRes.features.forEach((feature: any) => {
+          let seg_id_nat = feature.properties.seg_id_nat
+          let name = `${feature.properties.GNIS_NAME} (${seg_id_nat})`
+          items.push({
+            name: name,
+            id: seg_id_nat,
+            category: 'stream segment',
+          })
+        })
+
+        hucRes.features.forEach((feature: any) => {
+          let hucId = feature.properties.huc8
+          let name = `${feature.properties.name} (${hucId})`
+          items.push({
+            name: name,
+            id: hucId,
+            category: 'huc',
+          })
+        })
+
+        items = $_.sortBy(items, ['name'])
+
+        items = $_.map(items, (item: { name: string; category: string }) => {
+          item.name =
+            item.name + ` <span class="category">${item.category}</span>`
+          return item
+        })
+
+        return items
+      },
+      keys: ['name'],
+    },
+    resultItem: {
+      highlight: true,
+    },
+    threshold: 3,
+    debounce: 200,
+  }
+
+  let searchAutoComplete = new $autoComplete(config)
+
+  searchAutoComplete.input.addEventListener('selection', function (event) {
+    let selection = event.detail.selection.value
+    let id = selection.id
+    let category = selection.category
+    if (category === 'stream segment') {
+      navigateTo(`/conus/${id}`)
+    } else if (category === 'huc') {
+      navigateTo(`/huc/${id}`)
+    }
+  })
+})
+</script>
+
+<template>
+  <section class="section">
+    <div class="container">
+      <input
+        id="search"
+        v-model="inputValue"
+        ref="searchInput"
+        style="width: 600px"
+      />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import '~/node_modules/@tarekraafat/autocomplete.js/dist/css/autoComplete.02.css';
+
+#search {
+  background-image: none;
+}
+
+:deep(.autoComplete_wrapper ul) {
+  z-index: 1000;
+}
+
+:deep(.autoComplete_wrapper > ul > li > .category) {
+  text-transform: uppercase;
+  display: inline-block;
+  padding-left: 1ex;
+  color: #888;
+  font-size: 90%;
+}
+</style>
--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
   <Intro />
+  <Search />
   <Map />
 </template>
