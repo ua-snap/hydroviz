5a2e0a546479e3d8633913e09397ae859111768d
Craig Stephenson <crstephenson@alaska.edu>
2026-03-06 14:54:41 -0900

    Use plotly.js-basic-dist-min instead of plotly.js-dist-min for 77% (5.7mb) reduction in library size.

--- webapp/components/Viz/MonthlyFlow.vue
+++ webapp/components/Viz/MonthlyFlow.vue
@@ -2,7 +2,7 @@
 import { watch, toRaw } from 'vue'
 import { getLayout, getConfig, initializeChart } from '~/utils/chart'
 const { $Plotly, $_ } = useNuxtApp()
-import type { Data } from 'plotly.js-dist-min'
+import type { Data } from 'plotly.js-basic-dist-min'
 
 const props = defineProps(['streamStats'])
 
