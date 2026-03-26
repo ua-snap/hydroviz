8ab692542eaae34d9df385fe5718afa0465f68b2
Craig Stephenson <crstephenson@alaska.edu>
2026-03-25 16:55:13 -0800

    Uncomment components that were commented out for testing.

--- webapp/components/Report.vue
+++ webapp/components/Report.vue
@@ -37,7 +37,7 @@ onUnmounted(() => {
           Statistics for {{ segmentName }}
           <span class="segmentId">ID{{ segmentId }}</span>
         </h3>
-        <!-- <ReportMap class="my-6" /> -->
+        <ReportMap class="my-6" />
         <div class="content is-size-5">
           Introduction to the report goes here. We can pull some summarized info
           about the specific stream segment in order to highlight aspects of
@@ -58,31 +58,31 @@ onUnmounted(() => {
       <div class="container">
         <h4 class="title is-4">Magnitude statistics</h4>
         <VizMonthlyFlow :stream-monthly-flow="streamMonthlyFlow" />
-        <!-- <StatsTable :stream-stats="streamStats" category="magnitude" /> -->
+        <StatsTable :stream-stats="streamStats" category="magnitude" />
       </div>
     </section>
     <section class="section">
       <div class="container">
         <h4 class="title is-4">Frequency statistics</h4>
-        <!-- <StatsTable :stream-stats="streamStats" category="frequency" /> -->
+        <StatsTable :stream-stats="streamStats" category="frequency" />
       </div>
     </section>
     <section class="section">
       <div class="container">
         <h4 class="title is-4">Duration statistics</h4>
-        <!-- <StatsTable :stream-stats="streamStats" category="duration" /> -->
+        <StatsTable :stream-stats="streamStats" category="duration" />
       </div>
     </section>
     <section class="section">
       <div class="container">
         <h4 class="title is-4">Timing statistics</h4>
-        <!-- <StatsTable :stream-stats="streamStats" category="timing" /> -->
+        <StatsTable :stream-stats="streamStats" category="timing" />
       </div>
     </section>
     <section class="section">
       <div class="container">
         <h4 class="title is-4">Rate of change statistics</h4>
-        <!-- <StatsTable :stream-stats="streamStats" category="rate_of_change" /> -->
+        <StatsTable :stream-stats="streamStats" category="rate_of_change" />
       </div>
     </section>
   </div>
