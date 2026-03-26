052c6e44d099320d5f97b0517a79eb54c246728c
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-03-26 10:45:41 -0700

    Adjust toggle language per Jeremy

--- webapp/components/StickyToggle.vue
+++ webapp/components/StickyToggle.vue
@@ -6,23 +6,21 @@ let { appContext, appEra } = storeToRefs(streamSegmentStore)
 
 <template>
   <div class="container block content is-size-5">
-    <div class="content clamp">
-      <p>
-        This tool lets you explore this dataset in two modes: the
-        <strong>middle of the road</strong> shows the averages for a
-        middle-of-the-road climate future, and
-        <strong>future extremes</strong> show the size of variation between the
-        minimums and maximums for lower and higher emissions scenarios.
-        <NuxtLink to="/about/data"
-          >Read more about how these are defined</NuxtLink
-        >
-        and learn how to apply them in your work.
-      </p>
-      <p>
-        The full dataset for this stream location may be downloaded as a CSV
-        file, below, for further analysis.
-      </p>
-    </div>
+    <p>
+      This tool lets you explore this dataset in two modes: the
+      <strong>middle of the road</strong> shows the average across 13 climate
+      model futures, and <strong>future extremes</strong> show the size of
+      variation between the minimums and maximums for lower and higher emissions
+      scenarios.
+      <NuxtLink to="/about/data"
+        >Read more about how these are defined</NuxtLink
+      >
+      and learn how to apply them in your work.
+    </p>
+    <p>
+      The full dataset for this stream location may be downloaded as a CSV file,
+      below, for further analysis.
+    </p>
   </div>
   <div class="sticky-toggle block content is-size-5 py-3 container">
     <p>
