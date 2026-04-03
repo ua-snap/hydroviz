9fa3c9084f07afb127b51a3ee738021af0c647c5
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-04-03 13:56:31 -0700

    Add a footer

---
+++ webapp/components/Footer.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { computed } from 'vue'
+const year = computed(() => {
+  return new Date().getFullYear()
+})
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="section">
+      <div class="container content is-size-5">
+        <p>
+          Technology by the
+          <a href="https://www.snap.uaf.edu/" title="🥦🥦🥦"
+            >Scenarios Network for Alaska + Arctic Planning</a
+          >
+          at the
+          <a href="https://uaf-iarc.org">International Arctic Research Center</a
+          >.
+        </p>
+
+        <p>
+          Funding provided by the
+          <a href="https://secasc.ncsu.edu"
+            >Southeast Climate Adaptation Science Center</a
+          >
+          (SE CASC).
+        </p>
+
+        <p>
+          <strong
+            >Please contact
+            <a href="mailto:uaf-snap-data-tools@alaska.edu"
+              >uaf-snap-data-tools@alaska.edu</a
+            >
+            with questions or comments.
+          </strong>
+          <a href="https://uaf-snap.org">Visit the SNAP website</a>
+          to see all of our climate tools.
+        </p>
+        <p>
+          UA is committed to providing accessible websites.
+          <a href="https://www.alaska.edu/webaccessibility/"
+            >Learn more about UA's notice of web accessibility.</a
+          >
+          If we can help you access this website's content,
+          <a href="mailto:uaf-snap-data-tools@alaska.edu"
+            >please email us at uaf-snap-data-tools@alaska.edu</a
+          >.
+        </p>
+        <p>
+          Copyright &copy; {{ year }} University of Alaska Fairbanks. All rights
+          reserved.
+          <br />
+          The <a href="https://www.alaska.edu/">University of Alaska</a> is an
+          Equal Opportunity/Equal Access Employer and Educational Institution.
+          The University is committed to a
+          <a href="https://www.alaska.edu/nondiscrimination/"
+            >policy of non-discrimination</a
+          >
+          against individuals on the basis of any legally protected status.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background-color: oklch(0.951 0.2417 113.82);
+}
+</style>
