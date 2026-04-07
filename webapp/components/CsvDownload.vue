89d7da07395b44673268ae0a40fd22ce8da68249
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-04-07 16:47:18 -0700

    Adds widget to connect user to downloads, injects text into report.

---
+++ webapp/components/CsvDownload.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+const { $config } = useNuxtApp()
+import { useStreamSegmentStore } from '~/stores/streamSegment'
+const streamSegmentStore = useStreamSegmentStore()
+let { segmentId } = storeToRefs(streamSegmentStore)
+const props = defineProps({
+  streamId: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <span
+    >Download
+    <a
+      :href="
+        $config.public.snapApiUrl +
+        '/conus_hydrology/stats/' +
+        segmentId +
+        '?format=csv'
+      "
+      >complete modeled hydrologic statistics</a
+    >
+    or
+    <a
+      :href="
+        $config.public.snapApiUrl +
+        '/conus_hydrology/modeled_climatology/' +
+        segmentId +
+        '?format=csv'
+      "
+      >modeled daily streamflow climatologies</a
+    >
+    in CSV format for analysis in a spreadsheet.</span
+  >
+</template>
