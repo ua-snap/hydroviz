8835b19897bdd82b57bfdaf26c1e724165e2f3de
Craig Stephenson <crstephenson@alaska.edu>
2026-04-08 13:21:50 -0800

    Add intro text above each chart to mitigate awkward page jumps, and increase user-friendliness (#155)
    * add chart intros
    
    * Add chart intro(s) for polarscatter plots.
    
    * Adjust language
    
    ---------
    
    Co-authored-by: Bruce Crevensten <bruce.crevensten@gmail.com>

--- webapp/components/Report.vue
+++ webapp/components/Report.vue
@@ -42,16 +42,76 @@ onUnmounted(() => {
     <StickyToggle />
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p v-if="appContext == 'mid'">
+            The chart below is a hydrograph that shows the modeled historical
+            mean (white line in center) and range of variation (gray band) with
+            the projected middle-of-the-road climate scenario&mdash;Stabilizing
+            High Emissions (RCP 6.0). The minimum and maximum across 13 climate
+            models are shown (the top and bottom lines), and the range of
+            variation for the means are shown as a shaded ribbon.
+          </p>
+          <p v-if="appContext == 'extremes'">
+            The charts below are hydrographs that show the modeled historical
+            mean (white line in center) and range of variation (gray band) for
+            two climate scenarios: Stabilizing Emissions (RCP 4.5) and
+            Increasing Emissions (RCP 8.5). The minimum and maximum across 13
+            climate models are shown in each chart (top and bottom lines), and
+            the range of variation for the means are shown as a shaded ribbon.
+          </p>
+        </div>
         <VizHydrograph :stream-hydrograph="streamHydrograph" />
       </div>
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p v-if="appContext == 'mid'">
+            The chart below shows the mean modeled monthly flow rate for the
+            historical modeled value (diamond) and with the projected
+            middle-of-the-road climate scenario&mdash;Stabilizing High Emissions
+            (RCP 6.0). The box plot show the range of values for 13 climate
+            models. Each box plot spans from quartile 1 to quartile 3, which is
+            the interquartile range (IQR). The second quartile&mdash;the
+            median&mdash;is marked by a line inside the box. The whiskers span
+            &#177;1.5 times the interquartile range, and outliers are shown as
+            colored dots.
+          </p>
+          <p v-if="appContext == 'extremes'">
+            The charts below show the mean modeled monthly flow rate for the
+            historical modeled value (diamond) with two climate scenarios:
+            Stabilizing Emissions (RCP 4.5) and Increasing Emissions (RCP 8.5).
+            The box plots show the range of values for 13 climate models. Each
+            box plot spans from quartile 1 to quartile 3, which is the
+            interquartile range (IQR). The second quartile&mdash;the
+            median&mdash;is marked by a line inside the box. The whiskers span
+            &#177;1.5 times the interquartile range, and outliers are shown as
+            colored dots.
+          </p>
+        </div>
         <VizMonthlyFlow :stream-monthly-flow="streamMonthlyFlow" />
       </div>
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p v-if="appContext == 'mid'">
+            The chart below shows the modeled annual maximum daily flow rate and
+            the date of its occurrence for the historical modeled value
+            (diamond) and with the projected middle-of-the-road climate
+            scenario, Stabilizing High Emissions (RCP 6.0), for 13 climate
+            models. This chart can help you see changes in the timing and
+            magnitude of annual maximum flow rates.
+          </p>
+          <p v-if="appContext == 'extremes'">
+            The charts below show the modeled annual maximum daily flow rate and
+            the date of its occurrence for the historical modeled value
+            (diamond) and for two climate scenarios, Stabilizing Emissions (RCP
+            4.5) and Increasing Emissions (RCP 8.5), for 13 climate models. This
+            chart can help you see changes in the timing and magnitude of annual
+            maximum flow rates.
+          </p>
+        </div>
         <VizMinMaxFlowDates
           :stream-min-max-flow-dates="streamMinMaxFlowDates"
         />
