a1b9b789488645ea1bc68175aa825f6dbafad80b
Craig Stephenson <crstephenson@alaska.edu>
2026-06-08 17:53:39 -0800

    Fix broken Leaflet marker image when webapp is built.

--- webapp/plugins/leaflet.client.ts
+++ webapp/plugins/leaflet.client.ts
@@ -1,4 +1,14 @@
 import L from 'leaflet'
+import markerIcon2xUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIconUrl from 'leaflet/dist/images/marker-icon.png'
+import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2xUrl,
+  iconUrl: markerIconUrl,
+  shadowUrl: markerShadowUrl,
+})
 
 export default defineNuxtPlugin(nuxtApp => {
   return {
