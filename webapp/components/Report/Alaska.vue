ac79d64b9cd8b8b44d2e261285bd3a6d9b6d4646
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-07-10 15:04:27 -0700

    Incorporate lost paragraphs from Craig's work with PR#251

--- webapp/components/Report/Alaska.vue
+++ webapp/components/Report/Alaska.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useStreamSegmentStore } from '~/stores/streamSegment'
+import { scenarioFullNames } from '~/types/modelsScenarios'
 const streamSegmentStore = useStreamSegmentStore()
 let {
   streamStats,
@@ -81,27 +82,82 @@ onUnmounted(() => {
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below is a hydrograph that shows the modeled historical
+            mean (white line in center) and range of variation (gray band) with
+            the projected scenario&mdash;{{ scenarioFullNames.ssp370 }}. The
+            minimum and maximum across four climate model runs are shown (the
+            top and bottom lines), and the range of variation for the means are
+            shown as a shaded ribbon.
+          </p>
+        </div>
         <VizHydrograph :stream-hydrograph="streamHydrograph" />
       </div>
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below shows the mean modeled monthly flow rate for the
+            historical modeled value (diamond) and with the projected
+            scenario&mdash;{{ scenarioFullNames.ssp370 }}. The box plot shows
+            the range of values for four climate model runs. Each box plot spans
+            from quartile 1 to quartile 3, which is the interquartile range
+            (IQR). The second quartile&mdash;the median&mdash;is marked by a
+            line inside the box. The whiskers span &#177;1.5 times the
+            interquartile range, and outliers are shown as colored dots.
+          </p>
+        </div>
         <VizMonthlyFlow :stream-monthly-flow="streamMonthlyFlow" />
       </div>
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below shows the modeled annual maximum daily flow rate and
+            the date of its occurrence for the historical modeled value
+            (diamond) and with the projected scenario,
+            {{ scenarioFullNames.ssp370 }}, for four climate model runs. This
+            chart can help you see changes in the timing and magnitude of annual
+            maximum flow rates.
+          </p>
+        </div>
         <VizMaxFlowDates :stream-max-flow-dates="streamMaxFlowDates" />
       </div>
     </section>
 
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below is a temperature hydrograph that shows the modeled
+            historical mean (white line in center) and range of variation (gray
+            band) with the projected scenario&mdash;{{
+              scenarioFullNames.ssp370
+            }}. The minimum and maximum across four climate model runs are shown
+            (the top and bottom lines), and the range of variation for the means
+            are shown as a shaded ribbon.
+          </p>
+        </div>
         <VizTemperatureHydrograph :stream-wt-hydrograph="streamWtHydrograph" />
       </div>
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below shows the mean modeled monthly temperature for the
+            historical modeled value (diamond) and with the projected
+            scenario&mdash;{{ scenarioFullNames.ssp370 }}. The box plot shows
+            the range of values for four climate model runs. Each box plot spans
+            from quartile 1 to quartile 3, which is the interquartile range
+            (IQR). The second quartile&mdash;the median&mdash;is marked by a
+            line inside the box. The whiskers span &#177;1.5 times the
+            interquartile range, and outliers are shown as colored dots.
+          </p>
+        </div>
         <VizMonthlyTemperature
           :stream-monthly-temperature="streamMonthlyTemperature"
         />
@@ -109,6 +165,16 @@ onUnmounted(() => {
     </section>
     <section class="section">
       <div class="container">
+        <div class="content clamp is-size-5 mb-6">
+          <p>
+            The chart below shows the modeled annual maximum temperature and the
+            date of its occurrence for the historical modeled value (diamond)
+            and with the projected scenario,
+            {{ scenarioFullNames.ssp370 }}, for four climate model runs. This
+            chart can help you see changes in the timing and magnitude of annual
+            maximum temperatures.
+          </p>
+        </div>
         <VizMaxTemperatureDates :stream-max-temp-dates="streamMaxTempDates" />
       </div>
     </section>
@@ -117,6 +183,13 @@ onUnmounted(() => {
       <div class="container">
         <h4 class="title is-4">Complete statistics</h4>
 
+        <div class="is-size-5 mb-6">
+          <p>
+            The projected values in the tables below show the median values
+            across the {{ scenarioFullNames.ssp370 }} climate scenario for four
+            climate model runs.
+          </p>
+        </div>
         <StatsTable
           :stream-stats="streamStats"
           category="magnitude"
