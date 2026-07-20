c82230e03aa6a8166fc4ba8e54c6fcf43669c3f9
Craig Stephenson <crstephenson@alaska.edu>
2026-07-20 15:35:21 -0800

    Add a beta warning banner to every page of the webapp.

---
+++ webapp/components/BetaWarning.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section message is-warning mb-0 pb-3">
+    <div class="container">
+      <div class="message-header">
+        <p>This is a public beta test of this tool</p>
+      </div>
+      <div class="message-body">
+        Thanks for checking out this tool! Right now, this is a public beta,
+        which means that we&rsquo;re still gathering feedback, testing the data
+        outputs and fixing bugs. We&rsquo;d love to hear your feedback! Email us
+        at
+        <a href="mailto:uaf-snap-data-tools@alaska.edu"
+          >uaf&ndash;snap&ndash;data&ndash;tools@alaska.edu</a
+        >
+        if you encounter a problem.
+      </div>
+    </div>
+  </section>
+</template>
