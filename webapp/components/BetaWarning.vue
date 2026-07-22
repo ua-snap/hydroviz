a302f02dfc1e9c173875dff93aeccaa7c5d94f24
Bruce Crevensten <bruce.crevensten@gmail.com>
2026-07-22 11:31:29 -0700

    Quieter

--- webapp/components/BetaWarning.vue
+++ webapp/components/BetaWarning.vue
@@ -1,18 +1,14 @@
 <template>
   <section class="section message is-warning mb-0 pb-3">
     <div class="container">
-      <div class="message-header">
-        <p>This is a public beta test of this tool</p>
-      </div>
-      <div class="message-body">
-        Thanks for checking out this tool! Right now, this is a public beta,
-        which means that we&rsquo;re still gathering feedback, testing the data
-        outputs and fixing bugs. We&rsquo;d love to hear your feedback! Email us
-        at
+      <div class="message-body is-size-5">
+        Thanks for checking out this tool! Right now,
+        <strong>this is a public beta test</strong>, which means that
+        we&rsquo;re still gathering feedback, testing the data outputs and
+        fixing bugs. We&rsquo;d love to hear your thoughts! Email us at
         <a href="mailto:uaf-snap-data-tools@alaska.edu"
           >uaf-snap-data-tools@alaska.edu</a
-        >
-        if you encounter a problem.
+        >.
       </div>
     </div>
   </section>
